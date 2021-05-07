5457d1f069429a3e713d3bbb9078dae02e5ec374
Kronifer <>
2021-05-07 14:56:18 +0000

    added some shitty looking tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,50 +1,37 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/index.module.scss'
-
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Cat Dev Group</title>
-        <meta name="description" content="The site for the Cat Dev Group" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h2 className={styles.title}>
-          Welcome to the <a href="http://github.com/cat-dev-group">Cat Dev Group</a>!
-        </h2>
-
-        <p className={styles.description}>
-          This is a placeholder site until we code a full one. <br /> Until then, remember… <em>it's all objects (and also ᓚᘏᗢs)</em>!
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://github.com/cat-dev-group/" className={styles.card}>
-            <h2>GitHub &rarr;</h2>
-            <p>Contribute to our open source repositories!</p>
-          </a>
-
-          <a href="https://discord.gg/yt8s6C33fw" className={styles.card}>
-            <h2>Discord &rarr;</h2>
-            <p>Feel free to join us on Discord for fun!</p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
-  )
+export default function index() {
+    return (
+          <body className="space-y-8 md:space-y-4 pl-2 pt-2">
+          <div className="bg-gray w-screen">
+            <h1 className="text-white font-semibold text-5xl text-center">
+              Welcome to the Cat Dev Group Website!
+            </h1>
+          <blockquote className="pt-4">
+          <p className="text-white font-bold text-center text-lg">
+            <em>Your official hub for Cat Spam.</em>
+          </p>
+          </blockquote>
+          <p className="pt-6 text-white font-semibold text-xl text-center">
+            We're currently under construction at the moment! We apologize for the inconvenience.
+          </p>
+          </div>
+          <div className="grid grid-rows-1 gap-2 place-items-center items-center">
+          <a href="https://discord.gg/yt8s6C33fw" target="_blank" className="bg-lightgray rounded-md w-2/6 hover:ringColor-blue-500 hover:ring-2 transform transition hover:scale-110">
+            <h2 className="font-semibold text-center text-white text-lg">
+            Discord &rarr;
+            </h2>
+            <p className="text-white text-center text-md">
+              Come join our Discord, and hang out with us!
+            </p>
+            </a>
+            <a href="https://github.com/cat-dev-group" target="_blank" className="bg-lightgray rounded-md w-2/6 hover:ringColor-blue-500 hover:ring-2 transform transition hover:scale-110">
+              <h2 className="font-semibold text-center text-white text-lg">
+                GitHub &rarr;
+              </h2>
+              <p className="text-white text-center text-md">
+                Take a look at our GitHub projects, or even contribute to them!
+              </p>
+              </a>
+            </div>
+            </body>
+    )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [],
+  darkMode: 'media',
+  theme: {
+      colors: {
+          gray: "#1e2121",
+          lightgray: "#23272a",
+          blue: colors.blue,
+          white: colors.white,
+          black: colors.black
+      },
+  },
+  variants: {
+    extend: {
+        ringColor: ['hover'],
+        ringWidth: ['hover'],
+        outline: ['hover']
+    },
+  },
+  plugins: [],
+}
