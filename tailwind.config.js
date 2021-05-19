9f33183dcfaf2e43f53d8930bcc0387801af2240
Restyled.io <commits@restyled.io>
2021-05-19 16:01:04 +0000

    Restyled by prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge : [ "**/*.tsx" ],
-  darkMode : "media",
-  theme : {
-    colors : {
-      gray : "#1e2121",
-      lightgray : "#23272a",
-      blue : colors.blue,
-      white : colors.white,
-      black : colors.black,
+  purge: ["**/*.tsx"],
+  darkMode: "media",
+  theme: {
+    colors: {
+      gray: "#1e2121",
+      lightgray: "#23272a",
+      blue: colors.blue,
+      white: colors.white,
+      black: colors.black,
     },
   },
-  variants : {
-    extend : {
-      ringColor : [ "hover" ],
-      ringWidth : [ "hover" ],
-      outline : [ "hover" ],
+  variants: {
+    extend: {
+      ringColor: ["hover"],
+      ringWidth: ["hover"],
+      outline: ["hover"],
     },
   },
-  plugins : [],
+  plugins: [],
 };
